7255cc6a360837139407cb62cad573a8d71cedd9
Pattani Varun Kiran <160588139+Varunpattani@users.noreply.github.com>
2026-06-16 08:46:26 +0530

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-
-void main()
+int main()
 {
     int v = 5;
-    // Adjacency matrix for:
-    // [[1,2], [0,2,3], [0,1,4], [1,4], [2,3]]
-
     int adjMat[5][5] = {
         {0,1,1,0,0},
         {1,0,1,1,0},
@@ -13,31 +9,24 @@ void main()
         {0,1,0,0,1},
         {0,0,1,1,0}
     };
-
     int visited[5] = {0};
     int que[5];
-    int f = 0, r = 0;
-
-    
+    int f = -1, r = -1;
     visited[0] = 1;
-    que[r++] = 0;
-
+    que[++r] = 0;
     printf("BFS Traversal: ");
-
-    while(f < r)
+    while (f < r)
     {
-        int current = que[f++];
-
+        int current = que[++f];
         printf("%d ", current);
-
-        for(int i = 0; i < v; i++)
+        for (int i = 0; i < v; i++)
         {
-            if(adjMat[current][i] == 1 && visited[i] == 0)
+            if (adjMat[current][i] == 1 && visited[i] == 0)
             {
                 visited[i] = 1;
-                que[r++] = i;
+                que[++r] = i;
             }
         }
     }
-
+    return 0;
 }
